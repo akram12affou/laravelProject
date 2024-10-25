2e09b8bec937850d739e64f8f318ba7855c6a413
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2024-10-25 11:53:55 +0100

    'creating-structure'

---
+++ app/resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <a href="{{ url('/') }}">Home</a>
+        <a href="{{ url('/about') }}">About</a>
+       <!-- @auth
+             <a >Logout</a>
+        @else
+            <a >Login</a>
+            <a >Register</a>
+        @endauth -->
+    </nav>
+</header>
